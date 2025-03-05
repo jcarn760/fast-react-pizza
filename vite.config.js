f5c259fd7ec4721297ae9ec19f43caf90691caf8
jcarn760 <joshuacarnahan@yahoo.com>
2025-03-05 13:30:23 -0500

    add changes to deploy on github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/fast-react-pizza",
   plugins: [react(), tailwindcss(), "prettier-plugin-tailwindcss"],
 });
