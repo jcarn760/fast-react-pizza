721479e1797569892e2b598b80702d7626568fdc
jcarn760 <joshuacarnahan@yahoo.com>
2025-03-05 13:38:21 -0500

    modify again for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./fast-react-pizza",
+  base: "./fast-react-pizza/",
   plugins: [react(), tailwindcss(), "prettier-plugin-tailwindcss"],
 });
